2bb5d0591e7e602685c5b2e8ed07397134c6ad93
sumityadu <167018941+sumityadu@users.noreply.github.com>
2024-04-14 23:10:47 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -49,7 +49,7 @@ if ENV:
         "START_IMG", ""
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "worldwide_friend_zone")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "SPEED_NETWORK1")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
@@ -87,7 +87,7 @@ if ENV:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
 else:
-    from MukeshRobot.config import Development as Config
+    from ken kenaki.config import Development as Config
 
     API_ID = Config.API_ID
     API_HASH = Config.API_HASH
